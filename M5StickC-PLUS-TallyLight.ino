aa56740f39c7d32477f28bc1834e428cf403194b
Keith Senti <ksenti@gmail.com>
2023-06-16 14:17:51 -0500

    cleanup

--- M5StickC-PLUS-TallyLight.ino
+++ M5StickC-PLUS-TallyLight.ino
@@ -1,23 +1,44 @@
 #include <M5StickCPlus.h>
 #include "NetworkModule.h"
+#include <millisDelay.h>
 
+millisDelay setTimeDelay;
+void setTime();
 
 void setup () {
 
     Serial.begin(115200);
     
     M5.begin();
+    setCpuFrequencyMhz(80); //Save battery by turning down the CPU clock
+    btStop();               //Save battery by turning off Bluetooth
+
     M5.Lcd.setTextSize(1.5);
     M5.Lcd.setRotation(3);
     M5.Lcd.println(F("Booting..."));
 
-    setCpuFrequencyMhz(80); //Save battery by turning down the CPU clock
-    btStop();               //Save battery by turning off Bluetooth
-
     setupWiFi();
- 
+    setTimeDelay.start(1000);
 }
 
 void loop () {
+
+    if (setTimeDelay.justFinished()) setTime();
+
+}
+
+
+void setTime() {
+    
+    configTime(-21600, 3600, "time.apple.com");
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        Serial.println(F("Failed to obtain time"));
+        setTimeDelay.repeat();
+        return;
+    }
+    
+    setTimeDelay.stop();
+    Serial.println(&timeinfo, "%A %B %d, %Y %H:%M:%S");
 
 }
