9e3663d569fcc1305d6adea74a697164d91de522
Keith Senti <ksenti@gmail.com>
2023-06-16 13:47:28 -0500

    alpha 1 - working wifi

---
+++ M5StickC-PLUS-TallyLight.ino
@@ -0,0 +1,22 @@
+#include <M5StickCPlus.h>
+#include "NetworkModule.h"
+
+
+void setup () {
+
+    Serial.begin(115200);
+    
+    M5.begin();
+    M5.Lcd.setTextSize(1.5);
+    M5.Lcd.setRotation(3);
+
+    setCpuFrequencyMhz(80); //Save battery by turning down the CPU clock
+    btStop();               //Save battery by turning off Bluetooth
+
+    setupWiFi();
+ 
+}
+
+void loop () {
+
+}
---
+++ NetworkModule.cpp
@@ -0,0 +1,186 @@
+#include <M5StickCPlus.h>
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <time.h>
+
+void WiFiEvent(WiFiEvent_t event);
+void saveParamCallback();
+
+
+void setupWiFi () {
+
+    WiFi.mode(WIFI_STA);
+    WiFi.onEvent(WiFiEvent);
+    
+    char deviceId[9];
+    char deviceName[33];
+    ultoa(WIFI_getChipId(), deviceId, 16);
+    strcpy(deviceName, "M5StickC-Plus-");
+    strcat(deviceName, deviceId);
+
+    WiFiManager wm;
+
+    std::vector<const char *> menu = {"wifi","info","param","sep","restart","exit"};
+    wm.setMenu(menu);
+    wm.setConfigPortalBlocking(false);    
+    wm.setDebugOutput(false);
+    wm.setSaveParamsCallback(saveParamCallback);
+    wm.setClass("invert"); // set dark theme
+    wm.setCountry("US");
+    
+    M5.Lcd.println(F("Starting WiFi..."));
+    if (!wm.autoConnect(deviceName)) {
+        M5.Lcd.println(F("Config Portal Active"));
+        while (wm.getConfigPortalActive()) {
+            wm.process();
+            M5.update();
+            if (M5.BtnA.wasReleased()) {
+                wm.stopConfigPortal();
+            }
+        }
+        M5.Lcd.println(F("Config Portal Closed"));
+    }
+
+    M5.Lcd.println(wm.getWLStatusString());
+    
+}
+
+
+void WiFiEvent(WiFiEvent_t event){
+  
+  //Serial.printf("[WiFi-event] event: %d\n", event);
+
+  switch (event) {
+      
+      /*case ARDUINO_EVENT_WIFI_READY: 
+          Serial.println("WiFi interface ready");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_SCAN_DONE:
+          Serial.println("Completed scan for access points");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_STA_START:
+          Serial.println("WiFi client started");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_STA_STOP:
+          Serial.println("WiFi clients stopped");
+          break;*/
+      
+      case ARDUINO_EVENT_WIFI_STA_CONNECTED:
+          
+          Serial.println(F("Connected to access point"));
+          M5.Lcd.println(F("Connected to access point"));
+          
+          //updateTime();
+          //setupWebSockets();  
+          
+          configTime(-21600, 3600, "time.apple.com");
+            struct tm timeinfo;
+            if (!getLocalTime(&timeinfo)) {
+                M5.Lcd.println("Failed to obtain time");
+                return;
+            }
+            Serial.println(&timeinfo, "%A, %B %d \n%Y %H:%M:%S");
+          
+          break;
+
+      case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+          Serial.println("Disconnected from WiFi access point");
+          break;
+      
+      /*case ARDUINO_EVENT_WIFI_STA_AUTHMODE_CHANGE:
+          Serial.println("Authentication mode of access point has changed");
+          break;*/
+      
+      case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+          Serial.print("Obtained IP address: ");
+          Serial.println(WiFi.localIP());
+          break;
+      
+      case ARDUINO_EVENT_WIFI_STA_LOST_IP:
+          Serial.println("Lost IP address and IP address is reset to 0");
+          break;
+      
+      /*case ARDUINO_EVENT_WPS_ER_SUCCESS:
+          Serial.println("WiFi Protected Setup (WPS): succeeded in enrollee mode");
+          break;
+      
+      case ARDUINO_EVENT_WPS_ER_FAILED:
+          Serial.println("WiFi Protected Setup (WPS): failed in enrollee mode");
+          break;
+      
+      case ARDUINO_EVENT_WPS_ER_TIMEOUT:
+          Serial.println("WiFi Protected Setup (WPS): timeout in enrollee mode");
+          break;
+      
+      case ARDUINO_EVENT_WPS_ER_PIN:
+          Serial.println("WiFi Protected Setup (WPS): pin code in enrollee mode");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_AP_START:
+          Serial.println("WiFi access point started");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_AP_STOP:
+          Serial.println("WiFi access point  stopped");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_AP_STACONNECTED:
+          Serial.println("Client connected");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_AP_STADISCONNECTED:
+          Serial.println("Client disconnected");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_AP_STAIPASSIGNED:
+          Serial.println("Assigned IP address to client");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_AP_PROBEREQRECVED:
+          Serial.println("Received probe request");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_AP_GOT_IP6:
+          Serial.println("AP IPv6 is preferred");
+          break;
+      
+      case ARDUINO_EVENT_WIFI_STA_GOT_IP6:
+          Serial.println("STA IPv6 is preferred");
+          break;
+      
+      case ARDUINO_EVENT_ETH_GOT_IP6:
+          Serial.println("Ethernet IPv6 is preferred");
+          break;
+      
+      case ARDUINO_EVENT_ETH_START:
+          Serial.println("Ethernet started");
+          break;
+      
+      case ARDUINO_EVENT_ETH_STOP:
+          Serial.println("Ethernet stopped");
+          break;
+      
+      case ARDUINO_EVENT_ETH_CONNECTED:
+          Serial.println("Ethernet connected");
+          break;
+      
+      case ARDUINO_EVENT_ETH_DISCONNECTED:
+          Serial.println("Ethernet disconnected");
+          break;
+      
+      case ARDUINO_EVENT_ETH_GOT_IP:
+          Serial.println("Obtained IP address");
+          break;*/
+      
+      default: 
+        break;
+  }
+
+}
+
+
+void saveParamCallback () {
+
+}
